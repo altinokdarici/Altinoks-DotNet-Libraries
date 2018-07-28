a728a8d7e9f93fe0d4167fe14da62e8de0272b4c
Altinok Darici <altinok.darici@outlook.com>
2018-07-28 15:25:35 +0200

    Init

---
+++ Amsterdam.Http.Extensions/HttpClientExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Amsterdam.Http.Extensions
+{
+	public static class HttpClientExtensions
+	{
+
+		/// <summary>
+		///     Send a GET request to the specified Uri and deserialize the json response body as a <typeparamref name="T"/>
+		///     in an asynchronous operation.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="httpClient"></param>
+		/// <param name="requestUri"></param>
+		/// <returns></returns>
+		public static async Task<T> GetAsync<T>(this HttpClient httpClient, string requestUri)
+		{
+			var json = await httpClient.GetStringAsync(requestUri);
+			var result = JsonConvert.DeserializeObject<T>(json);
+
+			return result;
+		}
+
+		/// <summary>
+		///     Send a GET request and deserialize the json response body as a <typeparamref name="T"/>
+		///     in an asynchronous operation.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="httpClient"></param>
+		/// <param name="requestUri"></param>
+		/// <returns></returns>
+		public static Task<T> GetAsync<T>(this HttpClient httpClient) => httpClient.GetAsync<T>(string.Empty);
+	}
+}
