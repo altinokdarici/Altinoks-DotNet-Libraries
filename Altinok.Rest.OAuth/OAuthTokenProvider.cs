4f7f05b2af2b21b27d1325ab92db5acf5cb97c79
Altinok Darici <altinok.darici@outlook.com>
2018-10-13 12:02:42 +0300

    WIP: Rest.OAuth

---
+++ Altinok.Rest.OAuth/OAuthTokenProvider.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Rest;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Altinok.Rest.OAuth
+{
+	public class OAuthTokenProvider : ITokenProvider
+	{
+		private static HttpClient httpClient;
+		private static readonly object syncRoot = new object();
+		private readonly string tokenEndpoint;
+		private readonly string grantType;
+		private readonly IEnumerable<KeyValuePair<string, string>> properties;
+
+		public OAuthTokenProvider(string tokenEndpoint, string grantType, IEnumerable<KeyValuePair<string, string>> properties)
+		{
+			this.tokenEndpoint = tokenEndpoint;
+			this.grantType = grantType;
+			this.properties = properties;
+		}
+		public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
+		{
+			if (httpClient == null)
+			{
+				lock (syncRoot)
+				{
+					if (httpClient == null)
+					{
+						httpClient = new HttpClient();
+					}
+				}
+			}
+
+			var payload = new List<KeyValuePair<string, string>>
+			{
+				new KeyValuePair<string, string>("grant_type", grantType)
+			};
+			payload.AddRange(properties);
+
+
+			var content = new FormUrlEncodedContent(properties);
+			using (var response = await httpClient.PostAsync(tokenEndpoint, content))
+			{
+				response.EnsureSuccessStatusCode();
+				var json = await response.Content.ReadAsStringAsync();
+				var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(json);
+
+				return new AuthenticationHeaderValue(tokenResponse.TokenType, tokenResponse.AccessToken);
+
+			}
+		}
+
+	}
+}
---
+++ Altinok.Rest.OAuth/TokenResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Altinok.Rest.OAuth
+{
+	internal class TokenResponse
+	{
+		[JsonProperty("access_token")]
+		public string AccessToken { get; set; }
+
+		[JsonProperty("expires_in")]
+		public int ExpiresIn { get; set; }
+
+		[JsonProperty("token_type")]
+		public string TokenType { get; set; }
+	}
+}
